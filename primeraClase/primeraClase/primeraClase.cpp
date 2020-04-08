756d98c4cd734f8f85e0e17bcc271b5f574580b5
ItaloKanchaVilca <italo.kancha@ucsp.edu.pe>
2020-04-08 10:53:25 -0500

    añadi suma, resta,multiplicacion y division

--- primeraClase/primeraClase/primeraClase.cpp
+++ primeraClase/primeraClase/primeraClase.cpp
@@ -1,16 +1,36 @@
 // primeraClase.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
 //
 
-#include <iostream>
+#include <iostream> // entrada y salida de string
+using namespace std;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int a,b; //declaro mis variables
+	
+	cout << "Ingrese un muero : " << endl;
+	cin >> a;
+	cout << "Ingrese un numero : " << endl;
+	cin >> b;
+	
+	cout << "El valor de a es: "<<a << endl;
+	cout << "El valor de b es: "<<b << endl;
+
+	cout << "la suma de a y b es: " <<a+b<< endl;
+	cout << "la resta de a y b es: " << a - b << endl;
+	cout << "la multiplicacion de a y b es: " <<a*b<< endl;
+	cout << "la division de a y b es: " << a / b << endl;
+
+
+	return 0;
 }
 
 // Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
 // Depurar programa: F5 o menú Depurar > Iniciar depuración
-
+/*
+los comentarios sirven para hacer documentacion de mi codigo
+*/
 // Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
 //   2. Use la ventana de Team Explorer para conectar con el control de código fuente
 //   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
